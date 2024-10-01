c695ffc7b02106bf597cd269745e46394db49584
raulm-bzz <meiliraul2006@gmail.com>
2024-10-01 08:51:24 +0200

    initial commit

---
+++ Frontend/Charts.js
@@ -0,0 +1,25 @@
+import Chart from 'chart.js/auto';
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    let slot = document.getElementById("Chart");
+    new Chart(slot, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
