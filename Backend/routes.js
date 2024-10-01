f46966504f5fa04c1ce8b26962f98a57e9192ab4
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2024-10-01 13:12:45 +0200

    backand a bit polished and start frontend

---
+++ Backend/routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const entries = require('./schema/entrySchema.js');
+const mongo = require("mongodb")
+const {json} = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const result = await entries.find();
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+router.get("/:id", async (req, res)=>{
+    try {
+        const result = await entries.findOne({"_id": new mongo.ObjectId(req.params.id)});
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        delete req.body._id;
+        await entries.insertMany([req.body]);
+        res.sendStatus(200)
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+module.exports = router;
--- einleser.js
+++ einleser.js
@@ -1,9 +1,11 @@
+
+
 const fs = require('fs');
 const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser();
 
-fs.readFile('SDAT-Files/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml', 'utf8', (err, data) => {
+fs.readFile('./20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml', 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading the file:', err);
         return;
@@ -18,6 +20,5 @@ fs.readFile('SDAT-Files/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ES
         const jsonOutput = JSON.stringify(result, null, 2); // Pretty print JSON
         
         console.log(jsonOutput);
-
     });
 });
