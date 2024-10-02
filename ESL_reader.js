f5c013e49a4af4a75b96ff306256585ec77f0380
raulm-bzz <meiliraul2006@gmail.com>
2024-10-02 09:52:26 +0200

    read_ESL_ALL function to transform ALMOST all ESL files into JSON Files with only important information

--- ESL_reader.js
+++ ESL_reader.js
@@ -34,25 +34,48 @@ function read_ESL(file_path) {
                 }
             }
 
+            console.log(file_path)
             let meterData = jsonObject["ESLBillingData"]["Meter"][0]["TimePeriod"][0]
-            let EndDate = meterData["$"]
-            let final = EndDate
-            EndDate["end"] = EndDate["end"].split("T")[0]
-            final["End"] = final["end"]
-            delete final["end"]
-            let values = meterData["ValueRow"]
-            final.MeterReadings = [values[2]["$"], values[3]["$"], values[6]["$"], values[7]["$"]]
+            try{
 
-            fs.writeFile(`./ESL_Files/ESL_${EndDate["End"]}s.json`, JSON.stringify(final, null, 2), (error) => {
-                    if (error) {
-                        console.error(error);
-                        throw error;
+                let EndDate = meterData["$"]
+                let final = EndDate
+                EndDate["end"] = EndDate["end"].split("T")[0]
+                final["End"] = final["end"]
+                delete final["end"]
+                let values = meterData["ValueRow"]
+                final.MeterReadings = [values[2]["$"], values[3]["$"], values[6]["$"], values[7]["$"]]
+                fs.writeFile(`./ESL_Files/ESL_${EndDate["End"]}s.json`, JSON.stringify(final, null, 2), (error) => {
+                        if (error) {
+                            console.error(error);
+                            throw error;
+                        }
                     }
-                }
-            )
+                )
+            } catch (e){
+                console.log(e)
+            }
+
+
 
         })
     })
 }
 
-read_ESL("EdmRegisterWertExport_20190131_eslevu_20190322160349.xml")
+function read_ESL_all(dir_path){
+    fs.readdir(dir_path, (error, files) => {
+        if (error) {
+            return console.error('Error reading directory:', error);
+        }
+
+        // Iterate through each file
+        files.forEach(file => {
+            const filePath = path.join(dir_path, file);
+            read_ESL(filePath)
+
+        });
+    });
+}
+
+
+read_ESL_all("./ESL-Files")
